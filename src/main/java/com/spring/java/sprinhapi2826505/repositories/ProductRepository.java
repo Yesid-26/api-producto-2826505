5da56f8d1cff8c604e24cdc87242c4772abbb207
Ruhlian <julianospina250@gmail.com>
2024-07-24 17:21:04 -0500

    Repositorio Creado

---
+++ src/main/java/com/spring/java/sprinhapi2826505/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.spring.java.sprinhapi2826505.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.spring.java.sprinhapi2826505.entities.Product;
+
+public interface ProductRepository extends CrudRepository<Product , Long>{
+    //Repositorio por cada entidad
+    /
+
+}
